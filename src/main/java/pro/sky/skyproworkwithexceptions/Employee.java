4925650cd9c774dd4a008219494b3ee8c264ee22
L <EBogatyrevaPK@yandex.ru>
2022-03-26 13:17:34 +1200

    Initial commit

---
+++ src/main/java/pro/sky/skyproworkwithexceptions/Employee.java
@@ -0,0 +1,5 @@
+package pro.sky.skyproworkwithexceptions;
+
+public class Employee {
+
+}
---
+++ src/main/java/pro/sky/skyproworkwithexceptions/SkyProWorkWithExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproworkwithexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProWorkWithExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProWorkWithExceptionsApplication.class, args);
+	}
+
+}
