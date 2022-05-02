c5249cad8c29252252059dbd9bcbc4f939a212e2
L <EBogatyrevaPK@yandex.ru>
2022-05-02 22:38:41 +1200

    сделала по-другому)) и скажите все-таки кто здесь имеет зависимость которую мы должны глушить?) а то чем больше смотрю тем больше путаницы)

--- src/main/java/pro/sky/skyproworkwithexceptions/service/EmployeeGetByOficce.java
+++ src/main/java/pro/sky/skyproworkwithexceptions/service/EmployeeGetByOficce.java
@@ -4,9 +4,9 @@ import pro.sky.skyproworkwithexceptions.data.Employee;
 import pro.sky.skyproworkwithexceptions.exception.NotFoundException;
 
 import java.util.Collection;
-import java.util.List;
+
 import java.util.Map;
-import java.util.Optional;
+
 
 public interface EmployeeGetByOficce {
     Map<Integer, Employee> getEmployeeList();
--- src/test/java/pro/sky/skyproworkwithexceptions/service/impl/EmployeeByOfficeServiceImplTest.java
+++ src/test/java/pro/sky/skyproworkwithexceptions/service/impl/EmployeeByOfficeServiceImplTest.java
@@ -1,33 +1,51 @@
 package pro.sky.skyproworkwithexceptions.service.impl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.skyproworkwithexceptions.data.Employee;
+
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static pro.sky.skyproworkwithexceptions.service.impl.EmployeeConstant.EMPLOYEES_MAP;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeByOfficeServiceImplTest {
-
-
+    @Mock
+    private EmployeeByOfficeServiceImpl employeeByOfficeServiceMock;
     @InjectMocks
     private EmployeeByOfficeServiceImpl out;
 
     @Test
     void getEmployeeList() {
+        when(employeeByOfficeServiceMock.getEmployeeList())
+                .thenReturn(EMPLOYEES_MAP);
+        assertEquals(EMPLOYEES_MAP, out.getEmployeeList());
     }
 
     @Test
     void findEmployeeByOffice() {
+        when(employeeByOfficeServiceMock.findEmployeeByOffice(1))
+                .thenReturn((Collection<Employee>) EMPLOYEES_MAP);
+        assertEquals(EMPLOYEES_MAP, out.findEmployeeByOffice(1));
     }
 
     @Test
     void min() {
+        when(employeeByOfficeServiceMock.getEmployeeList())
+                .thenReturn(EMPLOYEES_MAP);
+        assertEquals(50_000, out.max(1));
+
     }
 
     @Test
     void max() {
-    }
-
-    @Test
-    void sortedEmployeeByOffice() {
+        when(employeeByOfficeServiceMock.getEmployeeList())
+                .thenReturn(EMPLOYEES_MAP);
+        assertEquals(30_000, out.min(1));
     }
 }
